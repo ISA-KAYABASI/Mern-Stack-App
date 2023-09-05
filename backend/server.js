6a9aa8d867b6a9f9439eda4d98ff161baeac8711
ISA-KAYABASI <dinyester.975@gmail.com>
2023-09-05 08:26:21 +0300

    start to doing login and signup modules

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutsRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
 
 
 // express app
@@ -18,6 +20,8 @@ app.use((req, res, next) =>{
 
 // routes
 app.use('/api/workouts', workoutsRoutes)
+app.use('/api/user', userRoutes)
+
 
 //connect to db
 mongoose.connect(process.env.MONGO_URI)
